c53604a9f19648cc4770cf804247d4f1e75ae810
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-14 05:19:33 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,22 +1,24 @@
 def consolidate_cart(cart)
-	consolidated = {}
+	consolidate = {}
 
 	cart.each do |item|
 		item_name = item.keys().first()
-		if consolidated.has_key?(item_name)
-			consolidated[item_name][:count] += 1
+		if consolidate.has_key?(item_name)
+			consolidate[item_name][:count] += 1
 		else
-			consolidated[item_name] = item[item_name]
-			consolidated[item_name][:count] = 1
+			consolidate[item_name] = item[item_name]
+			consolidate[item_name][:count] = 1
 		end
 	end
 
-	return consolidated
+	return consolidate
 end
 
 def apply_coupons(cart, coupons)
-  # require 'pry';binding.pry
-  
+  require 'pry';binding.pry
+  # coupon_cart = {}
+  #
+  # result coupon_cart
 end
 
 def apply_clearance(cart)

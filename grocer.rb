299252a8b408f938f2e7952964c9fc8b2254a0d3
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-14 05:41:56 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -20,9 +20,9 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
     if cart.has_key?(item)
       cart_item = cart[item]
-      require 'pry';binding.pry
-      cart["#{item} W/COUPON"] = {price: coupon[:cost], clearance: cart_item[:clearance], count: 1}
       cart_item[:count] -= coupon[:num]
+      # require 'pry';binding.pry
+      cart["#{item} W/COUPON"] = {price: coupon[:cost], clearance: cart_item[:clearance], count: 1}
     end
   end
 
